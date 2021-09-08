4a4ca366d79b693ab657e8b553a3951d1d7cb887
thaitrann <tranthai6693@gmail.com>
2021-09-08 20:03:56 +0700

    commit

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MYSQL_HOST =  'localhost'
-    MYSQL_USER =  'root'
-    MYSQL_PASSWORD =  '1234'
-    MYSQL_DB =  'excercise3'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'doan xem'
     
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USERNAME = 'testflaskmail005@gmail.com'
-    MAIL_PASSWORD = 'aA!123456'
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    # MYSQL_HOST =  'localhost'
+    # MYSQL_USER =  'root'
+    # MYSQL_PASSWORD =  '1234'
+    # MYSQL_DB =  'excercise3'
+    
+    # MAIL_SERVER = 'smtp.gmail.com'
+    # MAIL_PORT = 465
+    # MAIL_USERNAME = 'testflaskmail005@gmail.com'
+    # MAIL_PASSWORD = 'aA!123456'
+    # MAIL_USE_TLS = False
+    # MAIL_USE_SSL = True
