b8fdbbcdf4e887091670300fe9156d1e6031ff1c
thaitrann <tranthai6693@gmail.com>
2021-09-15 20:03:57 +0700

    create otp authentication for register

--- app/email/email.py
+++ app/email/email.py
@@ -2,8 +2,10 @@ from flask_mail import Message
 from app import mail, app
 from config import Config
 
+
 def sendMail(subject, email, content):
     message = Message(subject, sender = app.config['MAIL_USERNAME'], recipients = [email])
     message.body = content
     mail.send(message)
     return True
+
--- app/register/register.py
+++ app/register/register.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from wtforms.validators import Email
 from app.models.user import User
 from flask import Flask, render_template, redirect, flash, url_for, request
@@ -6,26 +7,26 @@ from .register_forms import RegisterForm
 from flask_login import current_user
 from app import db
 from app.email.email import sendMail
+from app.email.email_forms import AuthForm
 
-@app.route('/register', methods = ["GET","POST"])
+@app.route('/register', methods = ["GET", "POST"])  
 def register():
+    # content = 'test mail flask'
+    # sendMail('test mail flask shell', email, content)
     form = RegisterForm()
     if current_user.is_authenticated:
         return redirect(url_for('home'))
 
     if form.validate_on_submit():
-        content = 'test mail flask'
-        sendMail('test mail flask shell', form.email.data, content)
-
         user = User(email = form.email.data)
         user.setPassword(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-            
-        flash('You are now registered user')
-        return redirect(url_for('login'))
+        # db.session.add(user)
+        # db.session.commit()
+        return True
 
     return render_template('register.html', title = 'Register', form = form)
-        
 
+@app.route('/otp', methods = ["GET", "POST"])  
+def OTP():
+    return True
    
